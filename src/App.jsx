41adba01baa555fa8ef8a4c4ecb07efa42491c1a
Prachi Patkar <prachipatkar123@gmail.com>
2026-05-27 16:21:53 +0530

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
-import './App.css';
+import './footer.css';
 const App = () => {
   return (
     <div >
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
 const footer = () => {
   return (
     <>
@@ -9,52 +11,84 @@ const footer = () => {
         <div className="footer-box1">
           <h2>Nova Nest  <h3>Subscribe</h3>
           </h2>
-            
-                <p>Get 10% off your first order</p>
-            <div className="email-box">
 
-              <input
-                type="email"
-                placeholder="Enter your email"
-              />
+          <p>Get 10% off your first order</p>
+          <div className="email-box">
 
-              <button>
-                ➤
-              </button>
-            </div>
+            <input
+              type="email"
+              placeholder="Enter your email"
+            />
+
+            <button>
+              ➤
+            </button>
           </div>
-       
+        </div>
+
 
         <div className="footer-box2">
           <h2>Support </h2>
-             <p> 111 Bijoy sarani,Dhaka,<br />
+          <p> 111 Bijoy sarani,Dhaka,<br />
             DH 1515,Bangladesh.<br /><br />
             Nova Nest@gmail.com <br /><br />
             +88015-888888-99999
           </p>
-         
+
         </div>
 
 
         <div className="footer-box3">
           <h2>Account</h2>
-          <p>My Account <br/><br/>
-          Login/Register<br/><br/>
-          Cart<br/><br/>
-          Wishlist<br/><br/>
-          Shop<br/><br/>
+          <p>My Account <br /><br />
+            Login/Register<br /><br />
+            Cart<br /><br />
+            Wishlist<br /><br />
+            Shop<br /><br />
           </p>
-         
+
         </div>
-         <div className="footer-box4">
+        <div className="footer-box4">
           <h2>Quick Link</h2>
-          <p>Privacy policy <br/><br/>
-          Terms Of Use<br/><br/>
-          FAQ<br/><br/>
-          Contact<br/><br/>
-      
+          <p>Privacy policy <br /><br />
+            Terms Of Use<br /><br />
+            FAQ<br /><br />
+            Contact<br /><br />
+
           </p>
-         
+
+        </div>
+
+     
+
+        <div className="footer-box5">
+          <h2 >Download App</h2>
+          <p>Save $3 with App New User Only</p>
+       
+          <div className="app-flex-box">
+            <div className="qr-container">
+              <img 
+                src="public/QR imahe.webp" 
+                alt="QR Code" 
+              />
+            </div>
+            <div className="store-buttons">
+              <a href="#playstore">
+                <img src="public/f233865f2fff1a5cab5a148ce35fdf4ecf49fbf7.webp" alt="Google Play" />
+              </a>
+              <a href="#appstore">
+                <img src="public/badge_appstore-lrg.png" alt="App Store" />
+              </a>
+            </div>
+          </div>
+
+          {/* सोशल मीडिया आइकॉन्स */}
+          <div className="social-icons">
+            <a href="#fb"><FaFacebookF /></a>
+            <a href="#tw"><FaTwitter /></a>
+            <a href="#insta"><FaInstagram /></a>
+            <a href="#in"><FaLinkedinIn /></a>
+          </div>
         </div>
 
 
@@ -67,17 +101,7 @@ const footer = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-        
-{/* 
+        {/* 
         <div className="copy">
           <p>&copy; 2026 NovaNest. All Rights Reserved.</p>
         </div> */}
