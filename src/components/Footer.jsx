fb6c71990587c62ce817b262e808cdb787082267
Prachi Patkar <prachipatkar123@gmail.com>
2026-05-27 11:38:55 +0530

    currently working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,49 +4,65 @@ const footer = () => {
   return (
     <>
 
-    <footer className="footer">
-    
-      <div className="footer-box1">
-        <h4>Nova Nest  <h3>Subscribe</h3>
-        </h4>
-       <div className="email-box">
-        <p>Get 10% off your first order</p>
-  <input 
-    type="email" 
-    placeholder="Enter your email" 
-  />
+      <footer className="footer">
 
-  <button>
-    ➤
-  </button>
-</div>
-      </div>
+        <div className="footer-box1">
+          <h2>Nova Nest  <h3>Subscribe</h3>
+          </h2>
+            
+                <p>Get 10% off your first order</p>
+            <div className="email-box">
 
-      <div className="footer-box2">
-        <h4>Support</h4>
-       <p> 111 Bijoy sarani,Dhaka,<br/>
-DH 1515,Bangladesh.<br/><br/>
-Nova Nest@gmail.com <br/><br/>
-       +88015-888888-99999
-       </p>
-        
+              <input
+                type="email"
+                placeholder="Enter your email"
+              />
+
+              <button>
+                ➤
+              </button>
+            </div>
+          </div>
        
-       
-      </div>
+
+        <div className="footer-box2">
+          <h2>Support </h2>
+             <p> 111 Bijoy sarani,Dhaka,<br />
+            DH 1515,Bangladesh.<br /><br />
+            Nova Nest@gmail.com <br /><br />
+            +88015-888888-99999
+          </p>
+         
+        </div>
 
 
-      <div className="footer-box3">
-        <h5>Contact</h5>
-        <p>Email: support@gmail.com</p>
-        <p>Phone: +91 9876543210</p>
-      </div>
+        <div className="footer-box3">
+          <h2>Account</h2>
+          <p>My Account <br/><br/>
+          Login/Register<br/><br/>
+          Cart<br/><br/>
+          Wishlist<br/><br/>
+          Shop<br/><br/>
+          </p>
+         
+        </div>
+         <div className="footer-box4">
+          <h2>Quick Link</h2>
+          <p>Privacy policy <br/><br/>
+          Terms Of Use<br/><br/>
+          FAQ<br/><br/>
+          Contact<br/><br/>
+      
+          </p>
+         
+        </div>
+{/* 
+        <div className="copy">
+          <p>&copy; 2026 NovaNest. All Rights Reserved.</p>
+        </div> */}
 
-      <div className="copy">
-        <p>&copy; 2026 NovaNest. All Rights Reserved.</p>
-      </div>
-  
- 
-    </footer>
+
+      </footer>
 
 
 
