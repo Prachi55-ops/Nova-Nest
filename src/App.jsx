1818ea23911922a4b651a1583b4a82bc4e77dc41
Prachi Patkar <prachipatkar123@gmail.com>
2026-05-30 11:22:05 +0530

    create offer para

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Footer from './components/Footer'
 import './footer.css';
 import Navbar from './components/Navbar';
 import './navbar.css';
+import Language from './components/Language'
+import './language.css';
 const App = () => {
   return (
     <div >
-        <Navbar/>
-     <Footer/>
-   
+      <Language />
+      <Navbar />
+      <Footer />
+
     </div>
   )
 }
---
+++ src/components/language.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Language = () => {
+  return (
+    <div className='container '>
+        <div className='row offer-box '>
+            <div className="col-sm-1"></div>
+            <div className='col-sm-6 sale-para'>
+                <p>Summer Sale For All Swim Suits And Free Delivery - OFF 50%!</p>
+                </div>
+            <div className='col-sm-2'></div>
+            <div className='col-sm-2'></div>
+            <div className='col-sm-1'></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Language
