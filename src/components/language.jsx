5abfe17391ff3457ecbc68a042a0d00311ed4e95
Prachi Patkar <prachipatkar123@gmail.com>
2026-05-30 18:07:17 +0530

    top-bar select language work in remaining

--- src/components/language.jsx
+++ src/components/language.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 const Language = () => {
   return (
     <div className='container '>
-        <div className='row offer-box '>
-            {/* <div className="col-sm-1"></div> */}
-            <div className='col-sm-7 sale-para'>
-                <p>Summer Sale For All Swim Suits And Free Delivery - OFF 50%!</p>
-                <h4>ShopNow</h4>
-                </div>
-            <div className='col-sm-2'></div>
-            <div className='col-sm-2 select-language '>
-              <select className=" select-box" >
-  <option  className='select-option'selected>Change Language</option>
-  <option className='select-option' value="1">Hindi</option>
-  <option className='select-option' value="2">English</option>
-  
-</select></div>
-            <div className='col-sm-1'></div>
+      <div className='row offer-box '>
+        {/* <div className="col-sm-1"></div> */}
+        <div className='col-sm-7 sale-para'>
+          <p>Summer Sale For All Swim Suits And Free Delivery - OFF 50%!</p>
+          <h4>ShopNow</h4>
         </div>
-      
+        <div className='col-sm-2'></div>
+        <div className='col-sm-3 top-bar  '>
+          <select className='language-select'>
+            <option> English</option>
+            <option> Hindi</option>
+            <option> French</option>
+          </select>
+        </div>
+
+      </div>
+
     </div>
   )
 }
