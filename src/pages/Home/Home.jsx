fa7190bd48366e9946e065ff4effeca4a8cadf5b
Prachi Patkar <patkarp055@gmail.com>
2026-06-06 15:29:58 +0530

    woking on

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,68 +1,58 @@
 import React from 'react'
 import Topbar from '../../components/Topbar/Topbar'
 import "../../components/Topbar/topbar.css"
-import Navbar  from '../../components/Navbar/Navbar'
+import Navbar from '../../components/Navbar/Navbar'
 import "../../components/Navbar/navbar.css"
 import Footer from '../../components/Footer/Footer'
 import "../../components/Footer/footer.css"
+import { MdKeyboardArrowRight } from "react-icons/md";
+
 function Home() {
   return (
     <>
-    <Topbar/>
-    <Navbar/>
+      <Topbar />
+      <Navbar />
       <div className='container-home'>
         <div className='row home-box '>
-        
-            <div className='col-sm-1'></div>
-            <div className="col-sm-3 Side-bar">
-                <div className='Side-bar-box'>
-                    <ul className='fashion-list'>
-                    <li className="dropdown-button">
-  <a
-    className="nav-link dropdown-toggle"
-    href="#"
-    role="button"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-   Women's fashion
-  </a>
-  <ul className="dropdown-menu">
-    <li>
-      <a className="dropdown-item" href="#">
-        Action
-      </a>
-    </li>
-    <li>
-      <a className="dropdown-item" href="#">
-        Another action
-      </a>
-    </li>
-    <li>
-      <a className="dropdown-item" href="#">
-        Something else here
-      </a>
-    </li>
-  </ul>
-</li>
 
-                        <br/>
-                         <li> Men's Fashion</li>     <br/>
-                          <li> Electronics</li>     <br/>
-                           <li> Home & Lifestyle</li> <br/>
-                            <li> Medicine</li> <br/>
-                             <li> Sports & Outdoor</li> <br/>
-                             <li> Baby's & Toys</li> <br/>
-                             <li> Groceries & Pets</li> <br/>
-                             <li> Health & beauty</li> <br/>
+          <div className='col-sm-2'></div>
+          <div className="col-sm-3 Side-bar">
+            <div className='Side-bar-box'>
+              <ul className='fashion-list'>
+                <li > Women's Fashion
 
 
-                    </ul>
-                </div>
+                </li>
+                <br />
+                <li> Men's Fashion</li>     <br />
+                <li> Electronics</li>     <br />
+                <li> Home & Lifestyle</li> <br />
+                <li> Medicine</li> <br />
+                <li> Sports & Outdoor</li> <br />
+                <li> Baby's & Toys</li> <br />
+                <li> Groceries & Pets</li> <br />
+                <li> Health & beauty</li> <br />
+
+
+              </ul>
             </div>
+          </div>
+          <div className="col-sm-5 hero-box">
+            <div className='Row hero-section '>
+              <div className='col-sm-6 hero-content'> 
+                <h1 > hero section</h1>
+              </div>
+            <div className=' col-sm-6 hero-img'>
+                <img src='src/assets/images/iphone14pro-tradein.jpg'></img>
+            </div>
+
+          
+          </div>
+          </div>
+          <div className='col-sm-2'></div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   )
 }
