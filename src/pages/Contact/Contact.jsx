2a5ab2564d5ba1e0da1b2632a29c43c7f8b9d583
Prachi Patkar <patkarp055@gmail.com>
2026-06-11 16:16:00 +0530

    Working on Contact page UI

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import Topbar from '../../components/Topbar/Topbar'
+import '../../components/Topbar/topbar.css'
+import Navbar from '../../components/Navbar/Navbar'
+import '../../components/Navbar/navbar.css'
 function Contact() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Topbar/>
+      <Navbar/>
+       <div className='contact-container'>
+                <section className='section-box'>
+                    <div className=' contact-row1'>
+                        <div className='col-sm-12 '>
+                            <div className='contact-hightlight'>   <h4>Home </h4>
+                                <h4> /</h4>
+
+                                <h5 > Contact</h5> </div>
+
+                        </div>
+                    </div></section>
+                    <div className=" contact-row2">
+                      <div className="col-sm 1"></div>
+                      <div className="col-sm 3 contact-sml-call-box1">
+                        <div className='contact-sml-call-box2'></div>
+                      </div>
+                      <div className="col-sm 7 contact-big-box"></div>
+                      <div className="col-sm 1"></div>
+                    </div>
+                    </div>
+    </>
   )
 }
 
